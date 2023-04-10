597ffd2ee271560bcba464d1d5afea16f3dd2b6b
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2023-04-10 17:07:30 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import router from "./router.js";
+import fs from "fs";
+import https from "https";
+import cors from "cors";
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+
 app.use(router);
 
 app.listen(3000, console.log("Servidor iniciado na porta 3000"));
+
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3001, () => console.log('Servidor iniciado na porta 3001 em https'))
