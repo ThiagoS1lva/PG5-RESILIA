8c59acca1c333faf11bbf2f82076f25b569ed263
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2023-04-11 19:13:00 -0300

    Add files via upload

---
+++ src/models/Coletador.js
@@ -0,0 +1,10 @@
+class Coletador {
+    constructor(nome, email, cpf, data_nascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.data_nascimento = data_nascimento;
+    }
+}
+
+module.exports = Coletador
