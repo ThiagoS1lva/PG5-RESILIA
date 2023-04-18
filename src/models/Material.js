f786a551781a7fcb617098c52b4b292dc42f5dfe
Suzana Barbosa <115670321+SuzBarbosa@users.noreply.github.com>
2023-04-18 18:32:15 -0300

    Add files via upload

---
+++ src/models/Material.js
@@ -0,0 +1,10 @@
+class Material {
+    constructor(tipo, peso, quantidade, tamanho) {
+        this.tipo = tipo;
+        this.peso = peso;
+        this.quantidade = quantidade;
+        this.tamanho = tamanho;
+    }
+}
+
+module.exports = Material;
